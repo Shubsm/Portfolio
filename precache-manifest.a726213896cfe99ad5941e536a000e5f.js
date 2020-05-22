01cbe0457597645efcea67b293588097b04fcfb1
Shubham Mittal <mittalshuham600@gmail.com>
2020-05-22 13:53:39 +0530

    Updates

---
+++ precache-manifest.a726213896cfe99ad5941e536a000e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61a401ad48414264bab8303bd4f5ecf",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c8184f9ee0c5e6985821",
+    "url": "/Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2e56fca243cccd51d5a0",
+    "url": "/Portfolio/static/js/2.cf06da6c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Portfolio/static/js/2.cf06da6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8184f9ee0c5e6985821",
+    "url": "/Portfolio/static/js/main.fcdc9ec1.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  }
+]);
